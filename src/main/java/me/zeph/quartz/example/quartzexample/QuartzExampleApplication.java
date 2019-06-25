209450742b00f1b94f7cc2d363f997a9a68a40da
Benwei Zhu <benweizhu@gmail.com>
2019-06-25 19:54:25 +0800

    init

---
+++ src/main/java/me/zeph/quartz/example/quartzexample/QuartzExampleApplication.java
@@ -0,0 +1,13 @@
+package me.zeph.quartz.example.quartzexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuartzExampleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(QuartzExampleApplication.class, args);
+  }
+
+}
